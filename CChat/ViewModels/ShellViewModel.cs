cba4c0364de336738af5d12f073afa48aef86037
-Lunat1c- <arturgrabowski100@onet.pl>
2023-01-09 14:50:29 +0100

    Project setup for Caliburn.Micro

---
+++ CChat/ViewModels/ShellViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CChatClient.ViewModels
+{
+    internal class ShellViewModel : Conductor<object>
+    {
+        #region Properties
+
+        #endregion
+
+
+        #region Constructor
+        public ShellViewModel()
+        {
+
+        }
+        #endregion
+
+
+        #region Methods
+
+        #endregion
+    }
+}
