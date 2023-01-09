5470cfc175380d9c6c0a9e5780c9d62e2c4262c1
-Lunat1c- <arturgrabowski100@onet.pl>
2023-01-09 15:26:02 +0100

    Added Sidenav

---
+++ CChat/ViewModels/SidenavViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CChatClient.ViewModels
+{
+    internal class SidenavViewModel : Screen
+    {
+        #region Properties
+
+        #endregion
+
+
+        #region Constructor
+        public SidenavViewModel()
+        {
+
+        }
+        #endregion
+
+
+        #region Methods
+
+        #endregion
+
+
+        #region Button clicks
+
+        #endregion
+    }
+}
